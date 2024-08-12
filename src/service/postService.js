b79ad49462c7b0cf0fe6129b8852c98fb5f96f1d
Asif <mdasifhussain268@gmail.com>
2024-08-12 20:03:56 +0530

    updated time from utc to ist in post/list api

--- src/service/postService.js
+++ src/service/postService.js
@@ -7,28 +7,28 @@ const fs = require("fs")
 
 exports.postUpload = async (data, user_data) => {
     // console.log(data);
-    const {_id} = user_data
-    const {file} = data
+    const { _id } = user_data
+    const { file } = data
     // console.log(_id);
     const file_url = await cloudinary.uploader.upload(file.path)
     // console.log(file_url);
-    
+
     const upload_post = await postModel.create({
-        file_url : file_url.secure_url,
-        caption : data.caption,
-        user_id : _id
+        file_url: file_url.secure_url,
+        caption: data.caption,
+        user_id: _id
     })
-    
+
     if (upload_post) {
         const filePath = file.path
         // console.log(filePath);
-        
+
         fs.unlink(file.path, (err) => {
             if (err) {
-              console.error('Error deleting the file:', err);
+                console.error('Error deleting the file:', err);
             }
             // console.log('File deleted successfully!');
-          })
+        })
         return {
             success: 1,
             status: app_constant.SUCCESS,
@@ -41,13 +41,13 @@ exports.postUpload = async (data, user_data) => {
     return {
         success: 0,
         status: app_constant.INTERNAL_SERVER_ERROR,
-        message: 'Internal server error!', result: {} 
+        message: 'Internal server error!', result: {}
     }
-    
+
 }
 
 exports.getPostList = async (data) => {
-    const {id} = data
+    const { id } = data
     const _id = new mongoose.Types.ObjectId(id);
     // console.log(_id);
 
@@ -55,7 +55,7 @@ exports.getPostList = async (data) => {
     const offset = data.offset ? data.offset : 0
     const search = data.search ? data.search : ''
     // console.log(search);
-    
+
     let search_query = {}
 
     const pipeline = [
@@ -64,20 +64,33 @@ exports.getPostList = async (data) => {
     ]
 
     // const total_count = user_data.followers.length
-    
+
     if (search) {
         const regex = new RegExp(search, 'i')
-        search_query["$or"] = [{"caption" : regex}]
+        search_query["$or"] = [{ "caption": regex }]
     }
 
     const [result, total_count] = await Promise.all([
         postModel.aggregate([
             ...pipeline,
-            {$sort : {createdAt : -1}},
+            { $sort: { createdAt: -1 } },
+            { $unset: ["user_id", "__v"] },
             {
-                $project: {
-                    user_id : 0,
-                    __v : 0
+                $addFields: {
+                    createdAt: {
+                        $dateToString: {
+                            format: "%Y-%m-%d %H:%M:%S",
+                            date: "$createdAt",
+                            timezone: "Asia/Kolkata"
+                        }
+                    },
+                    updatedAt: {
+                        $dateToString: {
+                            format: "%Y-%m-%d %H:%M:%S",
+                            date: "$updatedAt",
+                            timezone: "Asia/Kolkata"
+                        }
+                    }
                 }
             },
             { $skip: +offset },
@@ -89,21 +102,22 @@ exports.getPostList = async (data) => {
         ])
     ])
     // console.log(result);
-    
+
     if (result) {
-        return { 
+        return {
             success: 1,
             status: app_constant.SUCCESS,
             message: 'Posts list fetched successfully!',
             total_count: total_count.length ? total_count[0].total_count : 0,
-            result 
+            result
         };
     }
 
-    return { 
+    return {
         success: 0,
         status: app_constant.INTERNAL_SERVER_ERROR,
         message: 'Internal server error!',
-        result: {} }
-    
+        result: {}
+    }
+
 }
